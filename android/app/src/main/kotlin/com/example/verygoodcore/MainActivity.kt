4a37d20cd5260903860e85c3985c3e51031b2ce8
Andres Eloy <andres.epacheco99@gmail.com>
2023-09-17 17:21:48 -0400

    feat: project initialized

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.eloy_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:eloy_dev/app/app.dart';
+import 'package:eloy_dev/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
